c71c120aae8e4e9c994f0c3f2df23da867944644
blaucomfuig <scorcuera6@gmail.com>
2021-05-21 19:30:50 +0200

    solved kata 'Sum All Numbers in a Range'

--- src/rangeArray.js
+++ src/rangeArray.js
@@ -1,7 +1,40 @@
 function sumAll(arr){
-    return 10;
+
+    let sortedArray = arr.sort((a,b) =>{
+        return a - b;
+    })
+
+   /*  console.log(sortedArray); */
+
+    let expandedArray = [];
+    for(let i = sortedArray[0]; i < sortedArray[1] +1; i++ ){
+        expandedArray.push(i);
+    }
+
+    /* console.log(expandedArray); */
+    
+    return expandedArray.reduce((a,b) =>{
+        return a + b;
+    })
+    
 }
+
 
 module.exports = sumAll;
 
-console.log(sumAll());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/rangeArray.test.js
+++ tests/rangeArray.test.js
@@ -1,5 +1,23 @@
 const sumAll = require("../src/rangeArray")
 
-test("given an array [4, 1] it should return the sum of all the numbers in between the range (1+2+3+4), 10", () =>{
-    expect(sumAll([4, 1])).toBe(10);
+test("if we pass [4, 1] to the function it should return the total sum of the numbers in between the range: 1 +2 +3 +4 = 10", () =>{
+    expect(sumAll([4,1])).toBe(10);
+});
+
+test("if we pass [1, 13] to the function it should return the total sum of the numbers in between the range 1 + ... + 13 = 81", ()=>{
+    expect(sumAll([1,13])).toBe(91);
+});
+
+test("if we pass [10, 100] to the function it should return the total sum of the numbers in between the range 10 + ... + 100 = 5005", ()=>{
+    expect(sumAll([10,100])).toBe(5005);
 })
+
+
+
+
+
+
+
+
+
+
